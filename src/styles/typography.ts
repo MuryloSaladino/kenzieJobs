8e121e79f22ea02e9d0d4ed6e2cab97641ec8e83
Murylo Saladino <murylosaladino@gmail.com>
2023-07-03 20:17:07 -0300

    fix(src): js para ts arquivos

--- src/styles/typography.ts
+++ src/styles/typography.ts
@@ -1,42 +1,47 @@
 import styled from "styled-components";
 
-export const Title1 = styled.h1`
+interface ITypographyProps{
+    color?: string;
+    bold?: string;
+}
+
+export const Title1 = styled.h1<ITypographyProps>`
     font-size: 4rem;
     font-weight: 700;
     color: ${({color}) => color ? color : "var(--color-black)"};
 `
 
-export const Title2 = styled.h2`
+export const Title2 = styled.h2<ITypographyProps>`
     font-size: 2.75rem;
     font-weight: 700;
     color: ${({color}) => color ? color : "var(--color-black)"};
 `
 
-export const Title3 = styled.h3`
+export const Title3 = styled.h3<ITypographyProps>`
     font-size: 1.3125rem;
     font-weight: 700;
     color: ${({color}) => color ? color : "var(--color-black)"};
 `
 
-export const BoldAlert = styled.strong`
+export const BoldAlert = styled.strong<ITypographyProps>`
     font-size: 1rem;
     font-weight: 700;
     color: ${({color}) => color ? color : "var(--color-black)"};
 `
 
-export const MenuText = styled.b`
+export const MenuText = styled.b<ITypographyProps>`
     font-size: 1rem;
     font-weight: 700;
     color: ${({color}) => color ? color : "var(--color-black)"};
 `
 
-export const Label = styled.label`
+export const Label = styled.label<ITypographyProps>`
     font-size: 1.0625rem;
     font-weight: 400;
     color: ${({color}) => color ? color : "var(--color-blue)"};
 `
 
-export const Paragraph = styled.p`
+export const Paragraph = styled.p<ITypographyProps>`
     font-size: 1.125rem;
     font-weight: ${({bold}) => bold ? '700' : '400'};
     color: ${({color}) => color ? color : "var(--color-black)"};
