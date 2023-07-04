5c657cdbcbcd6df2bb004f2e008fbed63bc9a86f
Murylo Saladino <murylosaladino@gmail.com>
2023-07-04 12:04:13 -0300

    fix(components): proteção de rotas provisória

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,9 @@ import App from './App.tsx'
 import { BrowserRouter } from "react-router-dom"
 import { UserProvider } from "./providers/UserContext.tsx"
 
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
@@ -12,5 +15,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <App />
       </UserProvider>
     </BrowserRouter>
+    <ToastContainer/>
   </React.StrictMode>,
 )
