b268aa3d13cde0f87f9fc0ac3768266e0eca096e
Murylo Saladino <murylosaladino@gmail.com>
2023-07-04 12:23:57 -0300

    fix(pages): Home zerada

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,7 @@
-import { Footer } from "../../components/Footer";
-import { Navbar } from "../../components/Navbar";
-
 export function Home() {
     
     return(
         <>
-            <Navbar/>
-            <Footer/>
         </>
     )
 }
