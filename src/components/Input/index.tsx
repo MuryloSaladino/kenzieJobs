c5d46ee5434c4a0b3cf6bed9b62ad068d4290632
Murylo Saladino <murylosaladino@gmail.com>
2023-07-05 14:54:23 -0300

    fix(components): comentario removido

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,21 +1,5 @@
 import { StyledInput } from "./styles";
-import { UseFormRegisterReturn } from "react-hook-form";
-
 import { forwardRef, ForwardedRef } from "react";
-
-// interface IInputProps{
-//     placeholder: string;
-//     type?: string;
-//     register: UseFormRegisterReturn<string>;
-// }
-
-// export function Input({placeholder, type, register}:IInputProps) {
-//     return(
-//         <StyledInput type={type} placeholder={placeholder} {...register} />
-//     )
-// }
-
-// -------------------------------------------------------------
 
 interface IInputProps {
     error: { message: string } | null;
