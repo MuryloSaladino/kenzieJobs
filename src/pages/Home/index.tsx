96ed0456406eb4d948bbaf0b8eee978ed20fe822
FirminoKNZ <sktt522@gmail.com>
2023-07-06 17:22:22 -0300

    feat (pages):home adicionada

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,27 @@
+import { Navbar } from "../../components/Navbar";
+import image from "../../assets/Rectangle 3.png"
+import { AboutH2, AboutJobsDiv, AboutUs, H2Slogan, SectionVacancies, SloganDiv } from "./styles";
+
 export function Home() {
     
     return(
         <>
-            Aqui é a home
+            <Navbar/>
+            <section>
+            <SloganDiv>
+                <H2Slogan>Trabalho é na Jobs</H2Slogan>
+            </SloganDiv>
+            <AboutJobsDiv>
+                <AboutH2>Sobre a Jobs</AboutH2>
+                <AboutUs>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis, optio voluptate! Itaque fugit accusamus iure reiciendis quasi, odio molestiae eaque, nisi nihil et minus a voluptates. Dicta unde incidunt ratione.</AboutUs>
+                <figure>
+                    <img src={image} alt="Imagem ilustrativa" style={{width: "100%"}}/>
+                </figure>
+            </AboutJobsDiv>
+            </section>
+            <SectionVacancies>
+                <AboutH2>Confira nossas vagas</AboutH2>
+            </SectionVacancies>
         </>
     )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,1 +1,43 @@
 import styled from "styled-components";
+
+export const SloganDiv = styled.div`
+    background: var(--color-blue);
+    display: flex;
+    height: 253px;
+    align-items: center;
+    padding: 0px 20px;
+    justify-content: flex-start;
+`
+
+export const H2Slogan = styled.h2`
+    font-weight: 700;
+    font-size: 44px;
+    line-height: 53.64px;
+    color: var(--color-white);
+    width: 60%;
+`
+
+export const AboutJobsDiv = styled.div`
+    margin: 50px 20px;
+    display: flex;
+    gap: 30px;
+    flex-direction: column;
+`
+
+export const AboutH2 = styled.h2`
+    font-weight: 700;
+    font-size: 44px;
+    line-height: 53.64px;
+    color: var(--color-blue);
+`
+
+export const AboutUs = styled.p`
+    font-weight: 400;
+    font-size: 18px;
+    line-height: 36px;
+`
+
+export const SectionVacancies = styled.section`
+    margin: 0px 50px;
+    text-align: center;
+`
